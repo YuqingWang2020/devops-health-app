993b5568a43924ffc9c5d78333837a86ef62ba51
Yuqing <yuqingwang2020@gmail.com>
2026-05-16 16:12:40 +0000

    change service name in app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def root():
 
 @app.get("/health")
 def health():
-    return {"status":"healthy","service":"health-api"}
+    return {"status":"healthy","service":"health-api-demo"}
 
 @app.get("/crash")
 def crash():
